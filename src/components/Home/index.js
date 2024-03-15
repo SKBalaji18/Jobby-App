3bb726deccd36db7eefbcf22ac0d753eb4f03f46
Balaji S.K <skbalajiskb@gmail.com>
2024-03-15 12:51:28 +0000

    second commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,30 @@
+import {Link} from 'react-router-dom'
+import Header from '../Header'
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="home-bg-container">
+      <div className="home-text-container">
+        <h1 className="home-head">Find The Job That Fits Your Life</h1>
+        <p className="home-desc">
+          Millions of people are searching for jobs,salary information,company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+        <button className="home-button">
+          <Link className="nav-link" to="/jobs">
+            Find Jobs
+          </Link>
+        </button>
+        <Link className="job-btn-link" to="/jobs">
+          <button type="button" className="home-button">
+            Find Jobs
+          </button>
+        </Link>
+      </div>
+    </div>
+  </>
+)
+
+export default Home
